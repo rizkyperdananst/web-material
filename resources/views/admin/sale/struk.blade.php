5fa0bac9998e9a5ae8d6a0d59ee95eb10ced9569
rizkyperdananst <riskiperdana345@gmail.com>
2024-03-18 09:36:37 +0700

    Revisi struk

--- resources/views/admin/sale/struk.blade.php
+++ resources/views/admin/sale/struk.blade.php
@@ -8,6 +8,7 @@
     <style>
         body {
             font-family: Arial, sans-serif;
+            font-size: 22px;
             margin: 0;
             padding: 0;
             /* background-color: red; */
