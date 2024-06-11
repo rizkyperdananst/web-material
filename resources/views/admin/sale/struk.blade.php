a8442e5c1c143993c03d0aa2d1f0683b6c8127c8
rizkyperdananst <riskiperdana345@gmail.com>
2024-06-11 16:36:13 +0700

    commit-4

--- resources/views/admin/sale/struk.blade.php
+++ resources/views/admin/sale/struk.blade.php
@@ -8,7 +8,7 @@
     <style>
         body {
             font-family: Arial, sans-serif;
-            font-size: 25px;
+            font-size: 23px;
             margin: 0;
             padding: 0;
             /* background-color: red; */
